06274265249395beabeb39dae8cb65fd6cba3f3d
Newmsy <35931074+Newmsy@users.noreply.github.com>
2020-07-03 00:29:14 +0100

    This is an example of a heroku deployment.

---
+++ build/precache-manifest.b33efc2502ea240696d9dc86cf813698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0f5b24c3e266e14f145487bcd5666b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e948aa5288999f21376e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "daae709a6edf240803a7",
+    "url": "/static/js/2.7666b33b.chunk.js"
+  },
+  {
+    "revision": "7d0f9e1c08d3ca8cfcbc009f387cf1f4",
+    "url": "/static/js/2.7666b33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e948aa5288999f21376e",
+    "url": "/static/js/main.8b0fc835.chunk.js"
+  },
+  {
+    "revision": "97206dd7530956f541cd",
+    "url": "/static/js/runtime-main.39ab20e5.js"
+  }
+]);
