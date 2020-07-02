d172f1f49d1a4a832dacd68dae743a9189b635d3
Newmsy <35931074+Newmsy@users.noreply.github.com>
2020-07-02 19:30:30 +0100

    importing router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Home } from './Pages/Home'
 import { Toolbar } from './Toolbar'
 import { Layout } from './Layout'
@@ -7,7 +13,6 @@ import { Layout } from './Layout'
 export default function App() {
   return (
     <div>
-    
     </div>
   );
 }
