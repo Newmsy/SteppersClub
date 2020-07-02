d4d4267e8bccc4d40754f6c66ce4ebef3fd8379e
Newmsy <35931074+Newmsy@users.noreply.github.com>
2020-07-02 19:20:34 +0100

    fixed?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import { Home } from './Pages/Home'
 import { Toolbar } from './Toolbar'
 import { Layout } from './Layout'
@@ -13,22 +7,7 @@ import { Layout } from './Layout'
 export default function App() {
   return (
     <div>
-    <Toolbar/>
-    <Layout>
-      <Router>
-          <Switch>
-            <Route path="/about">
-              <Home />
-            </Route>
-            <Route path="/users">
-              <Home />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-      </Router>
-    </Layout>
+    
     </div>
   );
 }
