9041f115100b6e7eadf6d154710878fde73b03aa
alex newman <alex.newmann@hotmail.com>
2020-07-31 13:49:08 +0100

    shop coming soon

---
+++ src/Pages/Shop/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { LogoMain } from '../LogoMain';
+
+const useStyles = makeStyles((theme) => ({
+    gridMainContentWrapper: {
+        width: '100%',
+        marginTop: 50,
+        direction: 'column',
+        flexGrow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyItems: 'center',
+    },
+    gridItemWrapper: {
+       
+    },
+    eventsComingSoonText: {
+        color: 'rgb(42, 0, 141)',
+        textShadow: '2px 2px rgb(255,209,76)',
+        fontWeight: 700,
+    },
+    eventsComingSoonWrapper: {
+        textAlign: 'center'
+    }
+}));
+  
+export function Shop() {
+    const styles = useStyles();
+    
+    return (
+        <Grid container className={styles.gridMainContentWrapper}>
+            <LogoMain/>
+            <Grid item container xs={12} className={styles.gridItemWrapper}>
+                <Grid item xs={12} className={styles.eventsComingSoonWrapper}>
+                    <h1 className={styles.eventsComingSoonText}>COMING SOON</h1>
+                </Grid>
+            </Grid>            
+        </Grid>
+        
+    )
+  }
+
+ 
