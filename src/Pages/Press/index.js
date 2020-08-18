f24b4cffb162b5b712935e1309a5e5cb6d8cd4bf
alex newman <alex.newmann@hotmail.com>
2020-08-18 13:30:41 +0100

    changes to fullscreen styles on press

--- src/Pages/Press/index.js
+++ src/Pages/Press/index.js
@@ -37,7 +37,8 @@ const useStyles = makeStyles((theme) => ({
     pressImageCover: {
         width: '100%',
         boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-        position: 'relative'
+        position: 'relative',
+        marginBottom: -5
     },
     pressIconGrid: {
         textAlign: 'center',
@@ -76,21 +77,28 @@ const useStyles = makeStyles((theme) => ({
     },
     soundcloudItem: {
         marginTop: 30,
-        marginBottom: 30
+        marginLeft: "7.5%"
     },
     imageHyperlink: {
         width:'100%',
-        marginBottom: -5
+        marginBottom: -10,
+        color: 'rgb(42, 0, 141)',
+        textShadow: '1px 1px rgb(255, 209, 76), 2px 2px rgb(42, 0, 141)',
     },
     subCoverCardsSolo: {
         backgroundColor: 'rgb(255, 209, 76)',
         borderBottomRightRadius: 30,
-        borderBottomLeftRadius: 30,
+        borderTopRightRadius: 30,
         boxShadow: '2px 2px 2px 2px rgb(42, 0, 141)',
         marginBottom: 70,
         justifyContent: 'center',
         alignItems: 'center',
         position: 'relative',
+        height: '100%',
+        [theme.breakpoints.down('sm')]: {
+            borderBottomLeftRadius: 30,
+            borderTopRightRadius: 0,
+        },
     }
 }));
   
@@ -104,37 +112,37 @@ export function Press() {
             <Grid item xs={12} style={{textAlign:'center'}}>
                 <h1 className={styles.pressHeader}>Press</h1>
             </Grid>
-            <Grid item container xs={12} sm={8} className={styles.pressIconGrid}>
-                <a href="https://soundcloud.com/steppersclub/reprezent-fm-steppers-club-guest-mix-and-interview-080820-zefer-w-meltout-crew" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
-                    <img className={styles.pressImageCover} src="/Assets/Images/ReprezentIcon.jpeg" alt="First Steps"/>
-                </a>
-            </Grid>
-            <Grid item container xs={12} sm={8} className={styles.subCoverCardsBack}>          
-                <Grid item container xs={12} className={styles.subCoverTitle}>
-                    REPREZENT FM GUEST APPEARANCE
+            <Grid xs={12} sm={10}  item container className={styles.subCoverCardsSolo}>
+                <Grid xs={12} md={3}>
+                    <a href="https://soundcloud.com/steppersclub/reprezent-fm-steppers-club-guest-mix-and-interview-080820-zefer-w-meltout-crew" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
+                        <img className={styles.pressImageCover} src="/Assets/Images/ReprezentIcon.jpeg" alt="Reprezent Radio"/>
+                    </a>
                 </Grid>
-                <Grid item container xs={12} className={styles.subCoverCardsFront}>
+                <Grid xs={12} md={9} item>
+                    <Grid item container xs={12} className={styles.subCoverTitle}>
+                        <a href="https://soundcloud.com/steppersclub/reprezent-fm-steppers-club-guest-mix-and-interview-080820-zefer-w-meltout-crew" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
+                            REPREZENT FM GUEST APPEARANCE
+                        </a>
+                    </Grid>
                     <Grid item xs={10} className={styles.soundcloudItem}>
                         <iframe title="Reprezent Soundcloud Player" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/873339163&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item container xs={12} sm={8} className={styles.pressIconGrid}>
-                <a href="https://www.cygnetmagazine.co.uk/cygnet-durham/steppers-club-the-new-face-of-the-uk-garage-scene" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
-                    <img className={styles.pressImageCover} src="/Assets/Images/CygnetPressIcon.png" alt="First Steps"/>
-                </a>
-            </Grid>
-            <Grid item container xs={12} sm={8} className={styles.subCoverCardsSolo}>          
-                
-                <Grid item container xs={12} className={styles.subCoverTitle}>
-                    CYGNET MAGAZINE FEATURE
+            <Grid xs={12} sm={10} item container className={styles.subCoverCardsSolo}>
+                <Grid xs={12} md={3}>
+                    <a href="https://www.cygnetmagazine.co.uk/cygnet-durham/steppers-club-the-new-face-of-the-uk-garage-scene" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
+                        <img className={styles.pressImageCover} src="/Assets/Images/CygnetPressIcon.png" alt="Cygnet Magazine"/>
+                    </a>
                 </Grid>
-                
+                <Grid xs={12} md={9} item>
+                    <Grid item container xs={12} className={styles.subCoverTitle}>
+                        <a href="https://www.cygnetmagazine.co.uk/cygnet-durham/steppers-club-the-new-face-of-the-uk-garage-scene" target="_blank" rel="noopener noreferrer" className={styles.imageHyperlink}>
+                            CYGNET MAGAZINE FEATURE
+                        </a>
+                    </Grid>
+                </Grid>
             </Grid>
-            
         </Grid>
-        
     )
-  }
-
- 
+}
