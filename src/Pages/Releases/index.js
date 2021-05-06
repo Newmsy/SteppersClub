9d04c1e74dfdb1db186dc52fe9d61337370d0d6b
alex <alex.newmann@hotmail.com>
2021-05-06 23:27:14 +0100

    shop revert and releases

--- src/Pages/Releases/index.js
+++ src/Pages/Releases/index.js
@@ -103,6 +103,224 @@ export function Releases() {
         <Grid item container xs={8} sm={4} className={styles.releaseItemGrid}>
           <img
             className={styles.firstStepsAlbumCover}
+            src="/Assets/Images/realSoonArt.jpg"
+            alt="First Steps"
+          />
+        </Grid>
+        <Grid item container xs={9} className={styles.subCoverTitle}>
+          STREAM/PURCHASE:
+        </Grid>
+        <Grid item container xs={7} className={styles.subCoverIcons}>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://soundcloud.com/steppersclub/minista-x-oppidan-x-sam-deeley-x-sensa-x-zefer-real-soon"
+            >
+              <img
+                src="/Assets/Images/soundcloudIcon.png"
+                alt="SOUNDCLOUD"
+                className={styles.socialMediaImageSoundcloud}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://open.spotify.com/track/7uHHr2BtvsNgHpwenCUHha"
+            >
+              <img
+                src="/Assets/Images/spotifyLogo.png"
+                alt="SPOTIFY"
+                className={styles.socialMediaImageSpotify}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.beatport.com/release/real-soon/3359055"
+            >
+              <img
+                src="/Assets/Images/beatportLogo.png"
+                alt="BEATPORT"
+                className={styles.socialMediaImageBeatport}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.junodownload.com/products/real-soon/5051842-02/"
+            >
+              <img
+                src="/Assets/Images/junoReleaseLogo.png"
+                alt="JUNO"
+                className={styles.socialMediaImageBeatport}
+              />
+            </a>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={8} className={styles.soundcloudItem}>
+          <iframe
+            title="soundcloudPlayer"
+            width="100%"
+            height="250"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1022514097&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+          <div
+            style={{
+              fontSize: "10px",
+              color: "#cccccc",
+              lineBreak: "anywhere",
+              wordBreak: "normal",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              fontFamily:
+                " Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+              fontWeight: 100,
+            }}
+          >
+            <a
+              href="https://soundcloud.com/steppersclub"
+              title="Steppers Club"
+              target="_blank"
+              style={{ color: " #cccccc", textDecoration: "none" }}
+            >
+              Steppers Club
+            </a>{" "}
+            ·{" "}
+            <a
+              href="https://soundcloud.com/steppersclub/minista-x-oppidan-x-sam-deeley-x-sensa-x-zefer-real-soon"
+              title="Minista x Oppidan x Sam Deeley x Sensa x Zefer - Real Soon"
+              target="_blank"
+              style={{ color: "#cccccc", textDecoration: "none" }}
+            >
+              Minista x Oppidan x Sam Deeley x Sensa x Zefer - Real Soon
+            </a>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid item container xs={12} justify="center">
+        <Grid item container xs={8} sm={4} className={styles.releaseItemGrid}>
+          <img
+            className={styles.firstStepsAlbumCover}
+            src="/Assets/Images/deeleyRelease.jpg"
+            alt="First Steps"
+          />
+        </Grid>
+        <Grid item container xs={9} className={styles.subCoverTitle}>
+          STREAM/PURCHASE:
+        </Grid>
+        <Grid item container xs={7} className={styles.subCoverIcons}>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://soundcloud.com/steppersclub/sets/sam-deeley-never-too-late-ep"
+            >
+              <img
+                src="/Assets/Images/soundcloudIcon.png"
+                alt="SOUNDCLOUD"
+                className={styles.socialMediaImageSoundcloud}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://open.spotify.com/album/5oAk28MOXGKvi1oXYM8BD7"
+            >
+              <img
+                src="/Assets/Images/spotifyLogo.png"
+                alt="SPOTIFY"
+                className={styles.socialMediaImageSpotify}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.beatport.com/release/never-too-late/3313020"
+            >
+              <img
+                src="/Assets/Images/beatportLogo.png"
+                alt="BEATPORT"
+                className={styles.socialMediaImageBeatport}
+              />
+            </a>
+          </Grid>
+          <Grid item xs={3} sm={2}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.junodownload.com/products/sam-deeley-caesar-sydney-never-too-late/5004158-02/"
+            >
+              <img
+                src="/Assets/Images/junoReleaseLogo.png"
+                alt="JUNO"
+                className={styles.socialMediaImageBeatport}
+              />
+            </a>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={8} className={styles.soundcloudItem}>
+          <iframe
+            title="soundcloudPlayer"
+            width="100%"
+            height="250"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1217586418&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+          <div
+            style={{
+              fontSize: "10px",
+              color: "#cccccc",
+              lineBreak: "anywhere",
+              wordBreak: "normal",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              fontFamily:
+                " Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+              fontWeight: 100,
+            }}
+          >
+            <a
+              href="https://soundcloud.com/steppersclub"
+              title="Steppers Club"
+              target="_blank"
+              style={{ color: " #cccccc", textDecoration: "none" }}
+            >
+              Steppers Club
+            </a>{" "}
+            ·{" "}
+            <a
+              href="https://soundcloud.com/steppersclub/sets/sam-deeley-never-too-late-ep"
+              title="Sam Deeley - Never Too Late EP"
+              target="_blank"
+              style={{ color: " #cccccc", textDecoration: "none" }}
+            >
+              Sensa - Deja Vu EP
+            </a>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid item container xs={12} justify="center">
+        <Grid item container xs={8} sm={4} className={styles.releaseItemGrid}>
+          <img
+            className={styles.firstStepsAlbumCover}
             src="/Assets/Images/DejaVuArtwork.png"
             alt="First Steps"
           />
